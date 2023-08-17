7c499b86a4aed811f3a030ca9b6d3e7bb2c40389
Amrit Kumar Mohanty <amritmohanty60@gmail.com>
2023-08-18 03:05:04 +0530

    refactor: build logic, added rows to table

--- app.js
+++ app.js
@@ -2,22 +2,34 @@ const billAmount = document.querySelector("#bill-amount")
 const cashAmount = document.querySelector('#cash-amount')
 const checkButton = document.querySelector('#button-check')
 const message = document.querySelector('#message')
+const noOfNotes = document.querySelectorAll(".notes")
+
+const availableDenominations = [2000,500,100,20,10,5,1]
 
 checkButton.addEventListener("click", function validateProcess(){
     message.style.display="none"
 
     if(cashAmount.value>=0 && billAmount.value>=0){
-        if(cashAmount.value>=billAmount.value){
-            showMessage("will do")
+        if(cashAmount.value >= billAmount.value){
+            // showMessage("will do")
+            const amountToBeReturned = cashAmount.value - billAmount.value;
+            calculateChange(amountToBeReturned)
         }
         else{
-            showMessage("insufficient cash")
+            showMessage("Insufficient cash")
         }
     }
     else{
-        showMessage("invalid")
+        showMessage("Please input valid positive amounts ONLY")
     }
 })
+
+function calculateChange(amountToBeReturned){
+    for(let i=0; i < availableDenominations.length ;i++){
+        noOfNotes[i].innerHTML = Math.trunc(amountToBeReturned/availableDenominations[i])
+        amountToBeReturned %= availableDenominations[i]
+    }
+}
 
 function showMessage(msg){
     message.style.display = "block"
