33b992498af523baca13ef232de20032b0bc5995
Amrit Kumar Mohanty <amritmohanty60@gmail.com>
2023-08-18 11:39:26 +0530

    style: small changes to padding and margin

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
 const billAmount = document.querySelector("#bill-amount")
 const cashAmount = document.querySelector('#cash-amount')
+const cashLabel = document.querySelector('label[for="cash-amount"]')
 const checkButton = document.querySelector('#button-check')
 const message = document.querySelector('#message')
 const noOfNotes = document.querySelectorAll(".notes")
+const table = document.querySelector("#cash-table")
 
 const availableDenominations = [2000,500,100,20,10,5,1]
+
+hideElement(cashAmount)
+hideElement(cashLabel)
+hideElement(table)
 
 checkButton.addEventListener("click", function validateProcess(){
     message.style.display="none"
 
-    if(cashAmount.value>=0 && billAmount.value>=0){
-        if(cashAmount.value >= billAmount.value){
+    if(billAmount.value>0){
+
+        showElement(cashAmount)
+        showElement(cashLabel)
+        checkButton.innerHTML = "Return Change"
+
+        if(cashAmount.value >= billAmount.value && cashAmount.value.typeof != "string"){
             // showMessage("will do")
             const amountToBeReturned = cashAmount.value - billAmount.value;
             calculateChange(amountToBeReturned)
+            showElement(table)
         }
         else{
-            showMessage("Insufficient cash")
+            if(cashAmount.value == "") showMessage("Waiting for cash input....")
+            else showMessage("Invalid Amount")
         }
     }
     else{
+        billAmount.amount
         showMessage("Please input valid positive amounts ONLY")
     }
 })
@@ -29,10 +43,18 @@ function calculateChange(amountToBeReturned){
         noOfNotes[i].innerHTML = Math.trunc(amountToBeReturned/availableDenominations[i])
         amountToBeReturned %= availableDenominations[i]
     }
+
 }
 
 function showMessage(msg){
     message.style.display = "block"
     message.innerHTML = msg
+}
+
+function showElement(element){
+    element.style.display = "block"
+}
+function hideElement(element){
+    element.style.display = "none"
 }
 
